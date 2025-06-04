26744e5fe1dbea7538a23c88bd2b503d5cf7fa79
chennammal <chennammal.s2021@kgkite.ac.in>
2025-06-04 12:47:28 +0530

    Initial commit

--- .history/supabase_setup_20250529113124.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    user_id UUID PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL,
-    registration_time TIMESTAMP NOT NULL,
-    last_login TIMESTAMP
-);
